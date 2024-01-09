53b020af88f0d5a18c100e34e3d969759ba6f7e7
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-10 01:28:12 +0700

    chore(zadark.rb): update version_scheme from 1 to 2 to align with current conventions chore(zadark.rb): comment out unused dependency on cmake to improve build process efficiency

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -8,7 +8,9 @@ class Zadark < Formula
   sha256 "fad29695dd3d81691df2e7316e27046fafb9319ca3871158fc316401ec96c4cd"
   license "MPL-2.0"
   version "12.5"
-  version_scheme 1
+  version_scheme 2
+
+  # depends_on "cmake" => :build
 
   def install
     bin.install "zadark"
