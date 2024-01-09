360a379ed54c25378e21cbddc4e431d9ee1d6f15
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-10 00:01:26 +0700

    chore(zadark.rb): update version number to 12.5 to match the downloaded release docs(zadark.rb): update documentation links to point to the correct resources

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -1,14 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
+# https://docs.brew.sh/Formula-Cookbook
+# https://rubydoc.brew.sh/Formula
+
 class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC"
   homepage "https://github.com/quaric/zadark"
   url "https://github.com/quaric/zadark/releases/download/24.1.1/zadark-12.5.tar.gz"
   sha256 "fad29695dd3d81691df2e7316e27046fafb9319ca3871158fc316401ec96c4cd"
   license "MPL-2.0"
-  version "24.1.2"
-
-  # depends_on "cmake" => :build
+  version "12.5"
 
   def install
     bin.install "zadark"
