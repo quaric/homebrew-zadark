b1fb2ad75e25dfb220ccd9b0c3afd32032377ec0
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-10 00:10:53 +0700

    chore(zadark.rb): update version number from 12.5.1 to 12.5 fix(zadark.rb): remove unused env_disable command to improve code cleanliness and readability

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,15 +2,14 @@
 # https://rubydoc.brew.sh/Formula
 
 class Zadark < Formula
-  desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC"
+  desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   url "https://github.com/quaric/zadark/releases/download/24.1.1/zadark-12.5.tar.gz"
   sha256 "fad29695dd3d81691df2e7316e27046fafb9319ca3871158fc316401ec96c4cd"
   license "MPL-2.0"
-  version "12.5.1"
+  version "12.5"
 
   def install
     bin.install "zadark"
-    env_disable "HOMEBREW_NO_ENV_HINTS"
   end
 end
