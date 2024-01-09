147feb2b193d59097bf119c0755d439f63b2f4ea
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-09 23:49:21 +0700

    chore(zadark.rb): update zadark formula to version 24.1.2 with new sha256 checksum

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -4,9 +4,9 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC"
   homepage "https://github.com/quaric/zadark"
   url "https://github.com/quaric/zadark/releases/download/24.1.1/zadark-12.5.tar.gz"
-  sha256 "7790fc2f977e12f7bae40c819f6998fa3c27b2e0f5b5fd729ebb18e83ab9b8d8"
+  sha256 "fad29695dd3d81691df2e7316e27046fafb9319ca3871158fc316401ec96c4cd"
   license "MPL-2.0"
-  version "24.1.1"
+  version "24.1.2"
 
   # depends_on "cmake" => :build
 
