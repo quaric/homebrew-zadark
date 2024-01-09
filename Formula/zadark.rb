10fc52cf59574473b4f403f5ccfd180a4a69402a
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-09 23:28:38 +0700

    chore(zadark.rb): update version number from 12.5 to 24.1.1 to match the downloaded release

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -6,7 +6,7 @@ class ZaDark < Formula
   url "https://github.com/quaric/zadark/releases/download/24.1.1/zadark-12.5.tar.gz"
   sha256 "7790fc2f977e12f7bae40c819f6998fa3c27b2e0f5b5fd729ebb18e83ab9b8d8"
   license "MPL-2.0"
-  version "12.5"
+  version "24.1.1"
 
   # depends_on "cmake" => :build
 
