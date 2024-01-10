e4a4b55351e06db817fa07f425c3a24ffcd4d3e8
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-01-10 13:46:23 +0700

    chore(zadark.rb): update sha256 checksum for zadark-macos-12.5.tar.gz The sha256 checksum for the zadark-macos-12.5.tar.gz file has been updated to b2912d41022e02853649e57a6918fb9edd32059fb6c5623613b13d69414f88ae. This update ensures the integrity of the file and reflects the changes made to the file.

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,7 +2,7 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   url "https://github.com/quaric/zadark/releases/download/24.1.2/zadark-macos-12.5.tar.gz"
-  sha256 "fad29695dd3d81691df2e7316e27046fafb9319ca3871158fc316401ec96c4cd"
+  sha256 "b2912d41022e02853649e57a6918fb9edd32059fb6c5623613b13d69414f88ae"
   license "MPL-2.0"
   version "12.5"
 
