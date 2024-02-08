35aa2dfacc8a18635957e359a6cb4e65df9c06c7
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-02-08 19:57:15 +0700

    chore(zadark.rb): update zadark version to 12.6 and download URL to the latest release to keep up with the latest changes and improvements

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -1,10 +1,10 @@
 class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
-  url "https://github.com/quaric/zadark/releases/download/24.1.2/zadark-macos-12.5.tar.gz"
-  sha256 "b2912d41022e02853649e57a6918fb9edd32059fb6c5623613b13d69414f88ae"
+  url "https://github.com/quaric/zadark/releases/download/24.2.1/zadark-macos-12.6.tar.gz"
+  sha256 "06fb5aad8bd41eed537581d6fda3e7abf6eb4acc5fb7c5894a82bb74c4cdabdf"
   license "MPL-2.0"
-  version "12.5"
+  version "12.6"
 
   def install
     bin.install "zadark"
