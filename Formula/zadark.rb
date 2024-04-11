71ca5cca68756661e0d165938402709e2c561fac
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-11 23:08:33 +0700

    fix(Formula/zadark.rb): refactor installation logic to properly handle different CPU architectures and stage the correct resource based on the architecture. This change ensures that the correct binary is installed based on the CPU architecture of the system.

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -7,15 +7,22 @@ class Zadark < Formula
   def install
     arch = Hardware::CPU.arch
 
-    case arch
-    when :arm64
-      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
-      sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
-    when :x86_64
+    resource "zadark-x86_64" do
       url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
       sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0"
     end
 
-    bin.install "zadark"
+    resource "zadark-arm64" do
+      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
+      sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
+    end
+
+    if arch == :arm64
+      resource("zadark-arm64").stage(stage_name: "zadark")
+    else
+      resource("zadark-x86_64").stage(stage_name: "zadark")
+    end
+
+    mv "zadark/zadark", bin/"zadark"
   end
 end
