fa095725ac3313cd613336dc1e8deb5880b1500d
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-11 23:02:22 +0700

    chore(zadark.rb): update Zadark formula to version 12.10.1 feat(zadark.rb): add support for arm64 architecture for Zadark installation feat(zadark.rb): add support for x86_64 architecture for Zadark installation

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -1,12 +1,21 @@
 class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
-  url "https://github.com/quaric/zadark/releases/download/24.4.1/zadark-macos-12.10.tar.gz"
-  sha256 "6e58cba28d75d26bc1d7ada13577a1ae9f895d6e69dc9d20b889834e93ac9871"
   license "MPL-2.0"
-  version "12.10"
+  version "12.10.1"
 
   def install
+    arch = Hardware::CPU.arch
+
+    case arch
+    when :arm64
+      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
+      sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
+    when :x86_64
+      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
+      sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0"
+    end
+
     bin.install "zadark"
   end
 end
