22595cc5bc15b6386bf0651b2b79d8556cc8cc07
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-11 23:11:48 +0700

    refactor(zadark.rb): update resource URL and sha256 based on CPU architecture to support arm64 and x64 platforms feat(zadark.rb): stage the zadark resource based on CPU architecture for proper installation

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -7,22 +7,27 @@ class Zadark < Formula
   def install
     arch = Hardware::CPU.arch
 
-    resource "zadark-x86_64" do
-      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
-      sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0"
+    resource "zadark" do
+      url if arch == :arm64
+        "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
+      else
+        "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
+      end
+      sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e" if arch == :arm64
+      sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0" if arch != :arm64
     end
 
-    resource "zadark-arm64" do
-      url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
-      sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
-    end
+    # resource "zadark-x86_64" do
+    #   url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
+    #   sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0"
+    # end
 
-    if arch == :arm64
-      resource("zadark-arm64").stage(stage_name: "zadark")
-    else
-      resource("zadark-x86_64").stage(stage_name: "zadark")
-    end
+    # resource "zadark-arm64" do
+    #   url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
+    #   sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
+    # end
 
+    resource("zadark").stage(stage_name: "zadark")
     mv "zadark/zadark", bin/"zadark"
   end
 end
