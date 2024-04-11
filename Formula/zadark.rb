445ee8e8acb6fc9649080e2db70f648c109a5108
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-11 23:25:08 +0700

    chore(zadark.rb): update download URL and sha256 checksum for x64 version to match the latest release version 24.4.2

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -8,7 +8,7 @@ class Zadark < Formula
     url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-arm64.tar.gz"
     sha256 "c63d3de1d9a026304e92416be208cd4195d768817700b9d3ebafd0b88c92221e"
   else
-    url "https://github.com/quaric/zadark/releases/download/24.4.1/zadark-macos-12.10-x64.tar.gz"
+    url "https://github.com/quaric/zadark/releases/download/24.4.2/zadark-macos-12.10.1-x64.tar.gz"
     sha256 "1a50877c32294e61dcb13d1cb7d1d94e6dadaa65d5887da2f20f09e1bf0348d0"
   end
 
