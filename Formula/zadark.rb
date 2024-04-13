f3d20cd97047da2864d11c8558eb898bd886356a
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-04-13 18:31:14 +0700

    chore(zadark.rb): refactor release_tag variable to improve code readability feat(zadark.rb): update download URLs to use dynamic release_tag and version variables for flexibility and maintainability

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -3,12 +3,13 @@ class Zadark < Formula
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
   version "12.11"
+  release_tag = "24.4.3"
 
   if Hardware::CPU.arm?
-    url "https://github.com/quaric/zadark/releases/download/24.4.3/zadark-macos-12.11-arm64.tar.gz"
+    url "https://github.com/quaric/zadark/releases/download/#{release_tag}/zadark-macos-#{version}-arm64.tar.gz"
     sha256 "4126efcf62a8edbfe04fb63c5328cb8733fa406b1f65c47633a2f4b6b62d8cb7"
   else
-    url "https://github.com/quaric/zadark/releases/download/24.4.3/zadark-macos-12.11-x64.tar.gz"
+    url "https://github.com/quaric/zadark/releases/download/#{release_tag}/zadark-macos-#{version}-x64.tar.gz"
     sha256 "fc9651e3c811bc14e1f1e729c20100dba25b7ed469e0633e514b4ed15b581ca0"
   end
 
