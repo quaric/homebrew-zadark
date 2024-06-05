9ffae2acf7e64e31885cb9604d938ea919f8fb3d
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-06-05 17:20:14 +0700

    feat(zadark.rb): update version to 12.14 and change download URLs to use storage.zadark.com for both arm64 and x64 architectures

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,15 +2,14 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
-  version "12.13"
-  release_tag = "24.6.1"
+  version "12.14"
 
   if Hardware::CPU.arm?
-    url "https://github.com/quaric/zadark/releases/download/#{release_tag}/zadark-macos-#{version}-arm64.tar.gz"
-    sha256 "25e1bb7f5e509fde7d67d17a50e02375fd0fedb5a57e61e4298069ac13b42b2b"
+    url "https://storage.zadark.com/zadark-macos-#{version}-arm64.tar.gz"
+    sha256 "d998fe909b1922574ff82f408d785f98a4090cbfbc6cbfffff0092a26cbb7947"
   else
-    url "https://github.com/quaric/zadark/releases/download/#{release_tag}/zadark-macos-#{version}-x64.tar.gz"
-    sha256 "7e652e1122e5ab36e10abac1024edec9876d013a1e34018eda82a1563ad29fdd"
+    url "https://storage.zadark.com/zadark-macos-#{version}-x64.tar.gz"
+    sha256 "e154b538810a351c33efd717f95da615a8e00a677d0fe7133e399b941ac12880"
   end
 
   def install
