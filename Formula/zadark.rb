347a7068403155c80820f4cdcd9a1806bfd7f4f1
Nguyễn Chánh Đại <dai@chanhdai.com>
2024-08-29 15:00:03 +0700

    chore(zadark.rb): update version from 14.0 to 14.0.1 and update sha256 checksums for arm64 and x64 architectures

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,14 +2,14 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
-  version "14.0"
+  version "14.0.1"
 
   if Hardware::CPU.arm?
     url "https://storage.zadark.com/zadark-macos-#{version}-arm64.tar.gz"
-    sha256 "7b5074df1428face188495407f2c454569a1c5098c094c3a976f9a26fca78c8f"
+    sha256 "64795d6604693a16b4d05e981d879fd808b4ff1e1698b1a3cf7e20b930e2d7cb"
   else
     url "https://storage.zadark.com/zadark-macos-#{version}-x64.tar.gz"
-    sha256 "2d999ae507ebea38995c28a60b4a40201b2ac29eccc3ccf40d6ebe67b7bb54eb"
+    sha256 "236f219fa03f8c0ff7e367f15287d6223de4d418c55e2ff13eb3a4867a73abfb"
   end
 
   def install
