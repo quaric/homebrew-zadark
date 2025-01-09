b0ade3d4051ee04d51998f845b88aa37b9207c32
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-01-09 20:24:23 +0700

    chore(zadark.rb): bump version from 15.1.6 to 15.1.7 to update formula fix(zadark.rb): update sha256 checksums for new version to ensure integrity

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,14 +2,14 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
-  version "15.1.6"
+  version "15.1.7"
 
   if Hardware::CPU.arm?
     url "https://storage.zadark.com/zadark-macos-#{version}-arm64.tar.gz"
-    sha256 "1e0a87a5941b78c8fc755b15cdead330a4e4584ca24c1df275adadce573e2728"
+    sha256 "50318212e504d5604eb3fb0ced590b5c8241c37b84dc158648d1b474e32a0aae"
   else
     url "https://storage.zadark.com/zadark-macos-#{version}-x64.tar.gz"
-    sha256 "82909d936fe3158058255a916e5a9421918899a5b0dbb1d0a9d01f1d08044f6a"
+    sha256 "01baff9e8fb363199ac7530ad3f8cb66a70205cde6fb5bdc4e0a2db3d2d78525"
   end
 
   def install
