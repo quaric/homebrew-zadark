df817c2fbca7cf49ab5dd99e46c621e8e8397124
Nguyễn Chánh Đại <dai@chanhdai.com>
2025-05-01 21:58:43 +0700

    chore(gitignore): add .DS_Store to ignore list to prevent macOS system files from being tracked
    fix(zadark.rb): update version to 15.2.3 to reflect the latest release
    
    fix(zadark.rb): change file extension from .tar.gz to .zip for both arm64 and x64 to match new distribution format
    
    fix(zadark.rb): update sha256 checksums to ensure integrity of the new .zip files

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,14 +2,14 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
-  version "15.2.2"
+  version "15.2.3"
 
   if Hardware::CPU.arm?
-    url "https://storage.zadark.com/zadark-macos-#{version}-arm64.tar.gz"
-    sha256 "d80d1921272db9cd1a846d29200959e6456a7a88f8d3bded17bdeb27f89c2b5e"
+    url "https://storage.zadark.com/zadark#{version}-macos-arm64.zip"
+    sha256 "c9b7c2c0ff64e9a57131f45a6d1e543b891f067c3c94c47b231f2f47a0feacf7"
   else
-    url "https://storage.zadark.com/zadark-macos-#{version}-x64.tar.gz"
-    sha256 "37e5945a99953d3040dbf159dd982aab4c79d64ad5c59b59799373bc19b2b516"
+    url "https://storage.zadark.com/zadark#{version}-macos-x64.zip"
+    sha256 "47f4c673fb3c1e83f0c9dc7bd6a732c2b63820ab0c1e6078c0135558f9dd6caa"
   end
 
   def install
