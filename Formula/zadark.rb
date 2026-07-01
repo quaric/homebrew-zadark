e4bffd7ce2133d2626f858b36ee2ad43eed819f7
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-07-01 13:03:19 +0700

    chore(zadark): bump version to 26.2.1 and update checksums for both arm64 and x64 macOS builds

--- Formula/zadark.rb
+++ Formula/zadark.rb
@@ -2,14 +2,14 @@ class Zadark < Formula
   desc "ZaDark is an extension that helps you enable Dark Mode for Zalo PC."
   homepage "https://github.com/quaric/zadark"
   license "MPL-2.0"
-  version "26.2"
+  version "26.2.1"
 
   if Hardware::CPU.arm?
     url "https://storage.zadark.com/zadark#{version}-macos-arm64.zip"
-    sha256 "7e0b6aa7a479a925f59ebdedd1ba7ac103712428737c732752a39ab59636e654"
+    sha256 "87b93727fe2567b4df5433bb32a497425b6a323c40c1e285d54161805ff0d849"
   else
     url "https://storage.zadark.com/zadark#{version}-macos-x64.zip"
-    sha256 "1f4fced7b4187d1c6e2b1723e418db426c3d18ad51d6e417dd4e73719bc5b15d"
+    sha256 "7d25f6c8c4f67d4d4ed0dc12620a036926edecc19719d0af3a1e980f879596ae"
   end
 
   def install
